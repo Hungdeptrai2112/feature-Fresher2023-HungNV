7aa6f9cef7414e10e844ab9b345dd26c764dc5a7
HungNVHE161056 <boyngheoghe0@gmail.com>
2026-01-29 10:14:52 +0700

    Add C# sample project

---
+++ implement_plus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        private double Calculate(SIGN sign, double arg1, double arg2)
+    {
+        switch (sign)
+        {
+            case SIGN.PLUS:
+                return arg1 + arg2;
+            default:
+                break;
+        }
+        return 0d;
+    }
+
+}
+}
